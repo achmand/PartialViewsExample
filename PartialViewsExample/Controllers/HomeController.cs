1e21615286a4bfefc3ba1a58c35ac9e2f3689516
achmand- Dylan Vassallo <dylan.vassallo@hotmail.com>
2017-01-27 23:00:05 +0100

    initial commit

---
+++ PartialViewsExample/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+using PartialViewsExample.Models;
+
+namespace PartialViewsExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var homeViewModel = new HomePageViewModel();
+            return View(homeViewModel);
+        }
+    }
+}
---
+++ PartialViewsExample/Models/CartShirtViewModel.cs
@@ -0,0 +1,44 @@
+﻿namespace PartialViewsExample.Models
+{
+    public sealed class ShirtOrderViewModel
+    {
+        public int Id { get; set; }
+        public string ShirtName { get; set; }
+    }
+
+    public sealed class Order
+    {
+        public int Id { get; set; }
+        public string OrderName { get; set; }
+    }
+
+    // Common in all pages
+    public class CartShirtViewModel
+    {
+        public ShirtOrderViewModel ShirtOrderViewModel { get; set; }
+        public Order Order { get; set; }
+    }
+
+    public sealed class HomePageViewModel : CartShirtViewModel
+    {
+        public string WelcomeMessage { get; set; }
+
+        public HomePageViewModel()
+        {
+            Order = new Order
+            {
+                Id=1,
+                OrderName = "Home Order"
+            };
+
+            ShirtOrderViewModel= new ShirtOrderViewModel
+            {
+                Id = 1,
+                ShirtName = "Home shirt"
+            };
+
+            WelcomeMessage = "welcome to home";
+        }
+    }
+
+}
